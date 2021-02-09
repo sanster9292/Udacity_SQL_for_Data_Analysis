7799785e1da79f367c0c1612a0b7d375fae3a708
sanwal-clarifai <60618046+sanwal-clarifai@users.noreply.github.com>
2021-02-09 00:28:02 -0500

    CASE quiz answers

--- Chapter_3/chapter-3-CASE.sql
+++ Chapter_3/chapter-3-CASE.sql
@@ -45,3 +45,28 @@ JOIN orders o
 ON a.id = o.account_id
 GROUP BY 1
 ORDER BY 2 DESC;
+
+#4 """We would now like to perform a similar calculation to the first, but we want
+ to obtain the total amount spent by customers only in 2016 and 2017. Keep the same
+ levels as in the previous question. Order with the top spending customers listed
+ first."""
+
+
+
+
+#5 """We would like to identify top performing sales reps, which are sales reps
+associated with more than 200 orders. Create a table with the sales rep name, the
+total number of orders, and a column with top or not depending on if they have more
+ than 200 orders. Place the top sales people first in your final table."""
+
+ SELECT s.name rep_name,
+        count(*) num_orders,
+        CASE WHEN count(*) > 200 THEN 'Top'
+        ELSE 'Not' END as top_or_not
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name
+ORDER BY num_orders DESC;
