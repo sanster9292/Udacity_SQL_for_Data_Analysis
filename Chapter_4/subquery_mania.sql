8c550d41f4c146bc278dd4ca60fa272e32f2552a
Sanwal Yousaf <sanwal5961@gmail.com>
2021-02-14 02:16:41 -0500

    added code for question 4

--- Chapter_4/subquery_mania.sql
+++ Chapter_4/subquery_mania.sql
@@ -55,6 +55,6 @@ ORDER BY COUNT(*);
 
 #3
 """
-How many accounts had more total purchases than the account name which has bought the most standard_qty
-paper throughout their lifetime as a customer?
+How many accounts had more total purchases than the account name which has bought the most
+standard_qty paper throughout their lifetime as a customer?
 """
