e393af7afa59184d110657ad26e71515699bee9d
Sanwal Yousaf <sanwal.yousaf@clarifai.com>
2021-03-26 19:34:56 -0400

    init for lesson 7 quizzes

---
+++ Lesson7_Advanced_SQL_joins/full_outer_join.sql
@@ -0,0 +1,13 @@
+LESSON 7
+QUIZ : Full Outer Join
+SQL SCHEMATIC : https://sanwal-bucket-01.s3.amazonaws.com/screen-shot-2017-11-16-at-3.54.06-pm.png
+
+
+#1
+"""
+Say you're an analyst at Parch & Posey and you want to see:
+
+    * each account who has a sales rep and each sales rep that has an account (all of the columns in these returned rows will be full)
+    * but also each account that does not have a sales rep and each sales rep that does not have an account (some of the columns in these returned rows will be empty)
+
+"""
